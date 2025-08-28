a5fc83cf72c691d80d4565ec00cca2c95b070f7a
Grandpa EJ <103351465+GrandpaEJ@users.noreply.github.com>
2025-08-28 18:40:30 +0000

    fix: Format __init__.py with black

--- python/puhu/__init__.py
+++ python/puhu/__init__.py
@@ -7,9 +7,24 @@ performance and memory-safety issues through a Rust backend.
 
 from .enums import ImageFormat, ImageMode, Resampling, Transpose
 from .image import Image
-from .operations import (blur, brightness, contrast, convert, crop,
-                         edge_detect, emboss, fromarray, new, open, paste,
-                         resize, rotate, save, sharpen, split)
+from .operations import (
+    blur,
+    brightness,
+    contrast,
+    convert,
+    crop,
+    edge_detect,
+    emboss,
+    fromarray,
+    new,
+    open,
+    paste,
+    resize,
+    rotate,
+    save,
+    sharpen,
+    split,
+)
 
 __version__ = "0.1.0"
 __author__ = "Bilal Tonga"
